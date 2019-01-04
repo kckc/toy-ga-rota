a8271510c48bb16a2e5fb89a6a06dab8c34d7cdc
Kenneth Chau <kckc.dev@gmail.com>
2019-01-04 11:34:52 +0000

    tslint fixes

--- src/util.ts
+++ src/util.ts
@@ -1,108 +1,113 @@
 import * as PEOPLE from './people';
-import CONFIG from './config';
+import { CONFIG } from './config';
 import { calculateScore } from './scoring';
 import { validateAll } from './validation';
 import * as _ from 'lodash';
 import { join } from 'path';
 
 function constructAvailabilities(): any[][] {
-    let availability: any[][] = [];
-    CONFIG.dates.forEach((date, i) => {
-        let roles = CONFIG.sessions.map((session) => {
+  let availability: any[][] = [];
+  CONFIG.dates.forEach((date, i) => {
+    const roles = CONFIG.sessions.map((session) => {
             // console.log(session, PEOPLE.roles, PEOPLE.roles['PA']);
-            return _.intersection(PEOPLE.roles[session[0]], PEOPLE.roles[session[1]], PEOPLE.peopleAvailability[i]);
-        });
+      return _.intersection(
+        PEOPLE.roles[session[0]], PEOPLE.roles[session[1]], PEOPLE.peopleAvailability[i]);
+    });
         // console.log(availability);
-        availability = availability.concat(roles);
-    })
-    return availability;
+    availability = availability.concat(roles);
+  });
+  return availability;
 }
 
-export var options = constructAvailabilities();
+export let options = constructAvailabilities();
 
 export function mutate(solution: string[], amount: number, fixed: number = 0) {
-    let mutate_index = _.times(amount, () => randomIndex(solution, fixed));
+  let mutateIndex = _.times(amount, () => randomIndex(solution, fixed));
 
-    if (mutate_index.length > solution.length)
-        mutate_index = _.take(mutate_index, solution.length);
+  if (mutateIndex.length > solution.length) {
+    mutateIndex = _.take(mutateIndex, solution.length);
+  }
 
-    let result = _.map(solution, _.clone);
-    
-    mutate_index.forEach((i) => {
-        result[i] = options[i][randomIndex(options[i])];
+  const result = _.map(solution, _.clone);
+
+  mutateIndex.forEach((i) => {
+    result[i] = options[i][randomIndex(options[i])];
+  });
+
+  while (!validateAll(result)) {
+    mutateIndex.forEach((i) => {
+      result[i] = options[i][randomIndex(options[i])];
     });
+  }
 
-    while (!validateAll(result))
-        mutate_index.forEach((i) => {
-            result[i] = options[i][randomIndex(options[i])];
-        });
-
-    return result;
+  return result;
 }
 
 export function crossover(solution1: string[], solution2: string[], limit: number) {
 
-    var s1: string[], s2: string[];
-    s1 = s2 = solution1.map(() => "");
-    var trial = 0,
-        newScore = 0, 
-        oldScore = calculateScore(solution1) + calculateScore(solution2);
-    while (!validateAll(s1) || !validateAll(s2) || oldScore < newScore)
-    {
-        let xaddress = _.times(CONFIG.crossoverPoints, ()=>randomIndex(solution1)).sort();
+  let s1: string[];
+  let s2: string[];
+  s1 = s2 = solution1.map(() => '');
+  let trial = 0;
+  let newScore = 0;
+  const oldScore = calculateScore(solution1) + calculateScore(solution2);
 
-        s1 = _.slice(solution1,0,xaddress[0]).concat(_.slice(solution2, xaddress[0]));
-        s2 = _.slice(solution2,0,xaddress[0]).concat(_.slice(solution1, xaddress[0]));
-        
-        _.tail(xaddress).forEach((address) => {
-            s1 = _.slice(solution1,0,address).concat(_.slice(solution2, address));
-            s2 = _.slice(solution2,0,address).concat(_.slice(solution1, address));
-        })
-        newScore = calculateScore(s1) + calculateScore(s2);
+  while (!validateAll(s1) || !validateAll(s2) || oldScore < newScore) {
+    const xaddress = _.times(CONFIG.crossoverPoints, () => randomIndex(solution1)).sort();
+
+    s1 = _.slice(solution1, 0, xaddress[0]).concat(_.slice(solution2, xaddress[0]));
+    s2 = _.slice(solution2, 0, xaddress[0]).concat(_.slice(solution1, xaddress[0]));
+
+    _.tail(xaddress).forEach((address) => {
+      s1 = _.slice(solution1, 0, address).concat(_.slice(solution2, address));
+      s2 = _.slice(solution2, 0, address).concat(_.slice(solution1, address));
+    });
+    newScore = calculateScore(s1) + calculateScore(s2);
 
         // console.log(s1, s2, solution1, solution2, xpoint);
-        if (trial++ > limit) return [solution1, solution2];
-    }
+    trial += 1;
+    if (trial > limit) return [solution1, solution2];
+  }
 
-    return [s1, s2];
+  return [s1, s2];
 }
 
 export function randomIndex(array: any[], from: number = 0) {
-    return Math.floor(Math.random() * (array.length - from)) + from;
+  return Math.floor(Math.random() * (array.length - from)) + from;
 }
 
 export function orderSameSession(solution: string[]) {
-    const sessionKey: {[key: string]: number[]} = CONFIG.sessions.reduce((prev, next, i) => {
-        const key = next.join('');
-        if (prev[key]) {
-            prev[key].push(i);
-        } else {
-            prev[key] = [i];
-        }
-        return prev;
-    }, {})
-
-    const sameSessionIndexes = Object.values(sessionKey).filter(k => k.length > 1);
-
-    if (sameSessionIndexes.length === 0) {
-        return solution;
+  const sessionKey: {[key: string]: number[]} = CONFIG.sessions.reduce((prev, next, i) => {
+    const key = next.join('');
+    if (prev[key]) {
+      prev[key].push(i);
+    } else {
+      prev[key] = [i];
     }
+    return prev;
+  },                                                                   {});
 
-    const reorder = (chunk: string[]) => {
-        let newOrder = [...chunk];
-        sameSessionIndexes.forEach((indexes: number[]) => {
-            const itemsToOrder = indexes.map(i => chunk[i]);
-            const order = itemsToOrder.sort();
-            indexes.forEach((_, i) => newOrder[indexes[i]] = order[i]);
-        })
-        return newOrder;
-    }
+  const sameSessionIndexes = Object.values(sessionKey).filter(k => k.length > 1);
 
-    const orderedSolution = [];
+  if (sameSessionIndexes.length === 0) {
+    return solution;
+  }
 
-    _.chunk(solution, CONFIG.sessions.length).forEach(chunk => {
-        orderedSolution.push(...reorder(chunk));
+  const reorder = (chunk: string[]) => {
+    const newOrder = [...chunk];
+    sameSessionIndexes.forEach((indexes: number[]) => {
+      const itemsToOrder = indexes.map(i => chunk[i]);
+      const order = itemsToOrder.sort();
+      indexes.forEach((_, i) => newOrder[indexes[i]] = order[i]);
     });
+    return newOrder;
+  };
 
-    return orderedSolution;
+  const orderedSolution = [];
+
+  _.chunk(solution, CONFIG.sessions.length).forEach((chunk) => {
+    orderedSolution.push(...reorder(chunk));
+  });
+
+  return orderedSolution;
 }
--- src/validation.ts
+++ src/validation.ts
@@ -1,35 +1,35 @@
-import CONFIG from './config';
+import { CONFIG } from './config';
 import * as PEOPLE from './people';
 import * as _ from 'lodash';
 
 export function validateAll(solution: string[]) {
-    return validateMethods.every(valid => valid(solution));
+  return validateMethods.every(valid => valid(solution));
 }
 
-var validateMethods = [
-    validateNameNotRepeatedInSameSession,
-    validatePeopleIsNotOverSetLimit
-]
+const validateMethods = [
+  validateNameNotRepeatedInSameSession,
+  validatePeopleIsNotOverSetLimit,
+];
 
 function validateNameNotRepeatedInSameSession(solution: string[]) {
-    let units = _.chunk(solution, CONFIG.sessions.length);
+  const units = _.chunk(solution, CONFIG.sessions.length);
 
-    let result = !units.some((u) => {
-        let count = _.filter(CONFIG.sessions, (s) => s[1] === "AM").length;
-        let am = _.take(u, count);
-        let pm = _.takeRight(u, u.length - am.length);
-        let isRepeated = (am.length !== _.uniq(am).length) || (pm.length !== _.uniq(pm).length);
+  const result = !units.some((u) => {
+    const count = _.filter(CONFIG.sessions, s => s[1] === 'AM').length;
+    const am = _.take(u, count);
+    const pm = _.takeRight(u, u.length - am.length);
+    const isRepeated = (am.length !== _.uniq(am).length) || (pm.length !== _.uniq(pm).length);
         // console.log(am, pm, isRepeated);
-        return isRepeated;
-    });
-    //console.log(solution, units, result);
-    return result;
+    return isRepeated;
+  });
+    // console.log(solution, units, result);
+  return result;
 }
 
 function validatePeopleIsNotOverSetLimit(solution: string[]) {
-    let count = _.countBy(solution);
+  const count = _.countBy(solution);
 
     // console.log(count, PEOPLE.limits);
 
-    return PEOPLE.limits.every((setLimit) => count[setLimit.name] <= setLimit.limit);
+  return PEOPLE.limits.every(setLimit => count[setLimit.name] <= setLimit.limit);
 }
